d6763360f51adaf81b408d2675669b39de8a2771
Benjamin Kaiser <benjaminjkaiser@gmail.com>
2021-10-22 00:14:27 +1000

    Finish creating draft of submit flow

--- saveRecording.js
+++ saveRecording.js
@@ -1,35 +1,39 @@
-const fs = require('fs');
-const path = require('path');
-const { books, chapters } = require('./data/books');
+import fs from 'fs';
+import { bookFiles, chapters } from './data/books.js';
 
 const BOOK = unescape(process.env.BOOK);
 const CHAPTER = parseInt(unescape(process.env.CHAPTER));
-const SPEAKER = unescape(process.env.SPEAKER);
-const GRAVATAR_HASH = unescape(process.env.GRAVATAR_HASH);
+const SPEAKER = process.env.SPEAKER !== undefined ? unescape(process.env.SPEAKER) : undefined;
+const GRAVATAR_HASH = process.env.GRAVATAR_HASH !== undefined ? unescape(process.env.GRAVATAR_HASH) : undefined;
 const VIDEOID = unescape(process.env.VIDEOID);
 
 function validId(id) {
-  return '/^[a-zA-Z0-9_-]{11}$/'.match(id);
+  return /^[a-zA-Z0-9_-]{11}$/.test(id);
 }
-if (!isValid(validId)) {
+if (!validId(VIDEOID)) {
   console.error("Video ID is invalid");
   process.exit(1);
 }
-
-if (SPEAKER.length < 1) {
+if (!SPEAKER) {
   console.error("Speaker must be specified");
   process.exit(1);
 }
 
-if (!books.includes(BOOK)) {
+if (!bookFiles.includes(BOOK)) {
   console.error("Book is not a valid book");
   process.exit(1);
 }
 const numberOfChapters = chapters[BOOK];
 
-if (CHAPTER < 1 || CHAPTER > numberOfChapters) {
+if (isNaN(CHAPTER) || CHAPTER < 1 || CHAPTER > numberOfChapters) {
   console.error("Chapter out of range for book specified");
   process.exit(1);
+}
+
+if (!GRAVATAR_HASH) {
+  console.error("Gravatar hash must be provided");
+  process.exit(1);
+
 }
 
 function escapeOutput(toOutput){
@@ -55,5 +59,5 @@ recordingsJson.push({
   videoId: VIDEOID
 });
 
-fs.writeFileSync('./data/recordings.json', JSON.stringify(recordingsJson));
+fs.writeFileSync('./data/recordings.json', JSON.stringify(recordingsJson, null, 2));
 
