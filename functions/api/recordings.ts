e5eb6fdd2fa685761565edd844635f0eaf407d84
Benjamin Kaiser <benjaminjkaiser@gmail.com>
2022-11-05 15:33:56 +1000

    Add example function that is failing to build

---
+++ functions/api/recordings.ts
@@ -0,0 +1,26 @@
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://mongo:<password>@thereadbible.iyfjpov.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+import { PrismaClient } from '@prisma/client/edge';
+const prisma = new PrismaClient();
+
+export async function onRequestGet(context): Promise<Request> {
+  // Contents of context object
+  const {
+    request, // same as existing Worker API
+    env, // same as existing Worker API
+    params, // if filename includes [id] or [[path]]
+    waitUntil, // same as ctx.waitUntil in existing Worker API
+    passThroughOnException, // same as ctx.passThroughOnException in existing Worker API
+    next, // used for middleware or to fetch assets
+    data, // arbitrary space for passing data between middlewares
+  } = context;
+  return prisma.recordings.findMany().then((recordings) => {
+    return new Response(JSON.stringify(recordings))
+  })
+}
